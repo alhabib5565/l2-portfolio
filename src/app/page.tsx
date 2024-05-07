aeab15ab89af58c9390dab1452c2d8fec2b07cda
alhabib5565 <alhabib5565@gmail.com>
2024-05-07 10:10:39 +0600

    setup about me and project\

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 import Hero from "./home/Hero";
-import Projects from "./home/Projects";
-import AboutMe from "./home/AboutMe";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-      <AboutMe />
-      <Projects />
     </div>
   );
 };
